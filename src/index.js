03acea3094027b3af51b404e8bfefbb1f5450b3b
Brian Lee <engbrianlee@gmail.com>
2021-08-09 13:35:45 -0400

    feat: createElement - jsx

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import * as React from "react";
 import ReactDOM from "react-dom";
 
-/**
- * A react element is just an object.
- * - element refer to react elements
- * - nodes refer to dom elements
- */
-const element = {
-  type: "h1",
-  props: {
-    title: "foo",
-    children: "hello"
-  }
-};
-
+const element = (
+  <div id="foo">
+    <a>bar</a>
+    <b />
+  </div>
+);
 const container = document.getElementById("root");
-
-const node = document.createElement(element.type);
-node["title"] = element.props.title;
-
-const text = document.createTextNode("");
-text["nodeValue"] = element.props.children;
-
-node.appendChild(text);
-container.appendChild(node);
+ReactDOM.render(element, container);
