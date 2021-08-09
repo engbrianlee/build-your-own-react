c60d7d7006428e28252b62ccd3e47fa1234d070a
Brian Lee <engbrianlee@gmail.com>
2021-08-09 13:26:28 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+/** Our first milestone: write the following in vanilla js */
+const element = <h1 title="foo">Hello</h1>;
+const container = document.getElementById("root");
+ReactDOM.render(element, container);
